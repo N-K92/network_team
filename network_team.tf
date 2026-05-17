dd8804e944a205acd0bb64a77cfc22c4e989f8d1
N-K92 <79423147+N-K92@users.noreply.github.com>
2026-05-17 17:56:05 +0200

    Update default public IP address in network_team.tf

--- network_team.tf
+++ network_team.tf
@@ -1,6 +1,6 @@
 variable "puplic_ips" {
     type = list(string)
-    default = [ "5.8.8.88", "1.1.1.1", "3.3.3.3", "4.4.4.44" ]  
+    default = [ "55.8.8.88", "1.1.1.1", "3.3.3.3", "4.4.4.44" ]  
 }
 
 output "public_ips" {
