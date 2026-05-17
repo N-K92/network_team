432088b55bd265881ea4fdfe09b4d130c1e09501
Kamel-schbock <kamelschbock@gmail.com>
2026-05-17 17:19:38 +0200

    first commit

---
+++ network_team.tf
@@ -0,0 +1,9 @@
+variable "puplic_ips" {
+    type = list(string)
+    default = [ "8.8.8.8", "1.1.1.1" ]  
+}
+
+output "public_ips" {
+  description = "public ip addresses"
+  value = var.puplic_ips
+}
