ffddcd759a3efd8a51d8e2a14592b09306f8d8ca
N-K92 <79423147+N-K92@users.noreply.github.com>
2026-05-17 17:39:38 +0200

    Add new public IP to the list of default IPs

--- network_team.tf
+++ network_team.tf
@@ -1,6 +1,6 @@
 variable "puplic_ips" {
     type = list(string)
-    default = [ "8.8.8.8", "1.1.1.1", "3.3.3.3" ]  
+    default = [ "8.8.8.8", "1.1.1.1", "3.3.3.3", "4.4.4.4" ]  
 }
 
 output "public_ips" {
