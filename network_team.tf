281d33f7086d7295ed86ade5c5227b69092307c7
N-K92 <79423147+N-K92@users.noreply.github.com>
2026-05-17 17:48:59 +0200

    Update network_team.tf

--- network_team.tf
+++ network_team.tf
@@ -1,6 +1,6 @@
 variable "puplic_ips" {
     type = list(string)
-    default = [ "8.8.8.8", "1.1.1.1", "3.3.3.3", "4.4.4.44" ]  
+    default = [ "8.8.8.88", "1.1.1.1", "3.3.3.3", "4.4.4.44" ]  
 }
 
 output "public_ips" {
